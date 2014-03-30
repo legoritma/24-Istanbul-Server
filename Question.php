1af1b8bfcb0929f9179ec69bb8bfc2064b71baad
ecearpacioglu <ecearpacioglu91@gmail.com>
2014-03-30 02:25:28 +0200

    Implementing /api/question/all.json

--- Question.php
+++ Question.php
@@ -4,7 +4,35 @@ class Question
 {
     public static function all()
     {
-        // return all questions
+        $questionsResults = getDatabase()->all('SELECT * FROM questions');
+        $questions = array();
+        foreach($questionsResults as $question)
+        {
+            $optionsResults = getDatabase()->all('SELECT * FROM options WHERE QuestionID = :id', array(':id' => $question['ID']));
+            $options = array();
+            foreach($optionsResults as $option)
+            {
+                  $options = array(
+                      'id' => $option['ID'],
+                      'text' => $option['Name'],
+                      'tag' => $option['TagID']
+                  );
+            }
+            $questions[] = array(
+                 'id' => $question['ID'],
+                 'question' => $question['Question'],
+                 'options' => $options,
+                 'update_date' => $question['LastUpdateDate']
+            );
+            
+        }
+        
+        $all = array(
+           'count' => count($questions),
+           'question' => $question
+        );
+        
+        return $all;
     }
 
     public static function updated($since)
