4a8381dfd1de053d3b67037adc4127fb6d736ebc
Onur BALCI <onurbalc@gmail.com>
2014-04-01 22:55:54 +0300

    Revert "poiler eklendi, kontrol edilmedi. "
    This reverts commit 5386176006a4f5774d7f0e30ff92b1bcd3695458.
    
    Reasons
    *Not tested commit (Check warnings at README)
    *All APIs down due to syntax error (most important reason)
    *Non-English comment
    *Non-English commit definition
    *PSR-2 not followed (whitespaces with tab, placement of baraces)

--- PlacesOfInterest.php
+++ PlacesOfInterest.php
@@ -5,50 +5,10 @@ class PlacesOfInterest
     public static function all()
     {
         // return all poi
-        
-        return PlacesOfInterest::updated(null);
     }
 
     public static function updated($since)
     {
-    	if (is_null($since)) 
-    	{
-    		$poiResults = getDatabase()->all('SELECT * FROM venues');
-    	} 
-    	else 
-    	{
-    		$poiResults = getDatabase()->all('SELECT * FROM venues WHERE LastUpdateDate > :since', array(':since' => $since));
-    	}
-    	$pois = array();
-    	foreach($poiResults as poi)
-    	{
-    		$tagsResults = getDatabase()->all('SELECT * FROM venue_meta WHERE venueID = :id', array(':id' => $poi['ID']));
-    		#önceki satýr venue_metadan doðru mu çekiyor bak. 
-    		$tags = array();
-    		foreach($tagsResults as $tag)
-    		{
-    			$tags[] = $tag['tagID'];
-    		}
-    		$pois[] = array(
-    			'id'			=> $poi['ID'];
-    			'name'			=> $poi['Name'];
-    			'lat'			=> $poi['Latitude'];
-    			'lng'			=> $poi['Longitude'];
-    			'tags'			=> $tags;
-    			'rating'		=> $poi['Rating'];
-    			'address'		=> $poi['Address'];
-    			'update_date'	=> $poi['LastUpdateDate'];
-    		)
-
-    		$all = array(
-    				'count' => count($pois),
-    				'pois' => $pois
-    		);
-    		
-    		return $all;
-    	}
-    	
         // return pois updated $since
-        
     }
 }
