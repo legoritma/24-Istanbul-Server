9990fcdbadc9dd743bbeb520b668c552239a033a
nickert <nick_hyves@Hotmail.com>
2014-05-02 10:21:13 +0300

    Update welcome.php

--- views/welcome.php
+++ views/welcome.php
@@ -3,7 +3,7 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <span class="glyphicon glyphicon-phone"></span>
+          <span class="glyphicon glyphicon-phon"></span>
           <h2>24Istanbul</h2>
           <p>The Android based APP that helps travelers all around the world to find there way in Istanbul. With over 80 locations, there is always a place to go.
             <p><a class="btn btn-lg btn-primary" href="about.html" role="button">View details &raquo;</a></p>
